b46824395645cf2d6c4796347f4f4ad1512bdbd5
danielhiji <danielhiji>
2025-01-03 14:51:55 +0000

    refactor constants into separate folder

---
+++ constants/index.ts
@@ -0,0 +1,67 @@
+// @ts-nocheck
+/* eslint-disable react-hooks/exhaustive-deps */
+
+// Main Navigation Links
+export const sidebarLinks = [
+  {
+    imgURL: "/icons/home.svg",
+    route: "/",
+    label: "Home",
+  },
+  {
+    imgURL: "/icons/microphone.svg",
+    route: "/create-podcast",
+    label: "Create",
+  },
+  {
+    imgURL: "/icons/discover.svg",
+    route: "/discover",
+    label: "Discover",
+  },
+];
+
+// Social Media Links
+export const socialLinks = [
+  {
+    imgURL: "https://iili.io/2Z2Aijs.md.png",
+    href: "https://github.com/danielhiji/SynthcastAI",
+    label: "GitHub",
+  },
+  {
+    imgURL: "https://iili.io/2Lh8XVa.png",
+    href: "https://x.com/synthcastai",
+    label: "Twitter",
+  },
+  {
+    imgURL: "https://iili.io/2UBgdjn.png",
+    href: "https://pump.fun/coin/",
+    label: "Pump.fun",
+  },
+];
+
+export const voiceDetails = [
+  {
+    id: 1,
+    name: "alloy",
+  },
+  {
+    id: 2,
+    name: "echo",
+  },
+  {
+    id: 3,
+    name: "fable",
+  },
+  {
+    id: 4,
+    name: "onyx",
+  },
+  {
+    id: 5,
+    name: "nova",
+  },
+  {
+    id: 6,
+    name: "shimmer",
+  },
+];
